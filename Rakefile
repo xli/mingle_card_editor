fcdcc8bdf1facd1475662980bd1989aaa8a7f6dd
Xiao Li <swing1979@gmail.com>
2012-04-08 23:47:08 -0700

    added simple chrome extension, ported mingle_wikimate as chrome ext; added chrome ext build script to create a tmp contains all files

--- Rakefile
+++ Rakefile
@@ -11,3 +11,16 @@ end
 task :c do
   `jison jison/wiki_parser.jison -o js/wiki_parser.js`
 end
+
+
+task :chrome => :clean do
+  mkdir_p 'tmp'
+  cp Dir['./../wikimate/public/javascripts/plugins/paragraph.js'], './tmp/'
+  cp Dir['./../wikimate/public/javascripts/wikimate.js'], './tmp/'
+  cp Dir['./chrome/*'], './tmp/'
+  cp Dir['./js/*'], './tmp/'
+end
+
+task :clean do
+  rm_rf 'tmp'
+end
---
+++ chrome/mingle_wikimate.js
@@ -0,0 +1,123 @@
+(function($) {
+  window.baseUrl = "/api/v2/projects/";
+  var editingCard = null;
+  var render_from_server_paragraph = {
+    emit: function(div, item) {
+      renderWiki(item.text, function(html) {
+        div.html(html);
+        div.unbind('dblclick').dblclick(function(e) {
+          e.stopPropagation();
+          return wikimate.plainTextEditor(div, item).focus();
+        });
+      });
+      return div.html('<img src="/images/spinner.gif" title="loading..."/>');
+    },
+    bind: function(div, item) {
+      return null;
+    }
+  }
+
+  $.extend(wikimate.plugins, {
+    paragraph: render_from_server_paragraph,
+    macro: render_from_server_paragraph,
+    body_macro: render_from_server_paragraph,
+    html: render_from_server_paragraph
+  });
+
+  var mingle_wiki_parser = {
+    parse: function(desc) {
+      function nextId(index) {
+        return 'story_item_' + index;
+      }
+      return _.map(wiki_parser.parse(desc), function(text, i) {
+        if (typeof(text) == 'string') {
+          return {id: nextId(i), type: 'paragraph', text: text};
+        } else {
+          return $.extend({id: nextId(i)}, text);
+        }
+      });
+    },
+  };
+
+  function renderWiki(content, onSuccess) {
+    var card_id = editingCard.find('card id').text();
+    var paramStr = $.param({
+      content_provider_x: {
+        id: card_id,
+        type: 'card'
+      },
+      content: content
+    });
+    jQuery.ajax({
+      url: window.baseUrl + project() + "/render?" + paramStr,
+      dataType: 'html',
+      success: onSuccess
+    });
+  };
+
+  function requestCard(uri, onSuccess) {
+    jQuery.ajax({
+      url: window.baseUrl + uri + '.xml',
+      dataType: 'xml',
+      success: onSuccess,
+      failure: function(x) {
+        console.log('failed: ' + x);
+      }
+    });
+  };
+
+  function renderWikiMate(story) {
+    $('#content').empty().wikimate({story: story, change: updateCardDescription});
+  };
+
+  function updateCardDescription(event, action) {
+    console.log("update: ");
+    console.log(action);
+  };
+
+  function parseCardDescription(description) {
+    return step('Parsing description', function() {
+      return mingle_wiki_parser.parse(description);
+    });
+  };
+
+  function loadCard(project, number) {
+    step('Loading ' + project + ' card #' + number + ' description', function() {
+      requestCard(project + '/cards/' + number, function(xmlDoc) {
+        editingCard = $(xmlDoc);
+        renderWikiMate(parseCardDescription(editingCard.find('card description').text()));
+      });
+    });
+  };
+
+  function cardPageMatch() {
+    return window.location.href.match(/\/projects\/([\da-z_]+)\/cards\/(\d+)$/);
+  };
+
+  function project() {
+    var match = cardPageMatch();
+    if (match) {
+      return match[1];
+    }
+    return undefined;
+  };
+  function number() {
+    var match = cardPageMatch();
+    if (match) {
+      return match[2];
+    }
+    return undefined;
+  };
+
+  function step(msg, func) {
+    $('#wiki').append(msg + "<br/>");
+    return func.call();
+  };
+
+  if (project() && number()) {
+    console.log("on card show page: " + project() + " #" + number());
+    loadCard(project(), number());
+  } else {
+    console.log("not on card show page");
+  }
+})(jQuery);
