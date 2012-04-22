632f3bdf9d58456d5f70d7cd74a6e24a9628fe3b
Xiao Li <swing1979@gmail.com>
2012-04-22 16:03:06 -0700

    added diff js, init journal for editor

--- chrome/mingle_wikimate.js
+++ chrome/mingle_wikimate.js
@@ -27,6 +27,51 @@
       $this.mingle_card_editor('status', 'error');
     }
 
+    var userIconCache = {};
+
+    function ajaxUpdateUserIcon(url, elements) {
+      $.ajax({
+        url: url,
+        dataType: 'xml',
+        success: function(xmlDoc) {
+          var icon_path = $(xmlDoc).find('icon_path').text();
+          if (icon_path) {
+            userIconCache[url] = icon_path;
+            updateUserIcon(url, elements);
+          }
+        },
+        failure: ajaxErrorHandler
+      });
+    }
+
+    function updateUserIcon(url, elements) {
+      if (userIconCache[url]) {
+        elements.css('background-image', 'url(' + userIconCache[url] + ')');
+      } else {
+        ajaxUpdateUserIcon(url, elements);
+      }
+    }
+
+    function currentUser() {
+      var currentUserId = $('#current-user a').attr('href').split('/')[3];
+      return "/api/v2/users/" + currentUserId + ".xml";
+    }
+
+    var actionImage = (function() {
+      var actionImages = {
+        "add": '/images/icon-source-add.png',
+        "edit": '/images/icon-edit-pencil.gif',
+        "remove": '/images/icon-source-delete.png',
+        "move": '/images/icon-source-modify.png'
+      }
+
+      return function (actionElement) {
+        return $('<img/>').attr('src', actionImages[actionElement.data('data').type])
+      }
+    })();
+
+    var user;
+
     return {
       init: function(card) {
         project = card.project;
@@ -47,11 +92,17 @@
 
       initWikiMate: function(cardDoc) {
         editingCard = $(cardDoc);
+        user = editingCard.find('card modified_by').attr('url');
         var story = mingle_wiki_parser.parse(editingCard.find('card description').text());
+        var journal = _.map(story, function(item) { return {id: item.id, type: 'add', item: item}; });
         var $this = this;
         this.empty().wikimate({story: story, change: function(event, action) {
           $this.mingle_card_editor('update', event, action);
-        }});
+        }}).wikimate('journal', journal, function(actionElement) {
+          updateUserIcon(user, actionElement);
+          actionElement.html(actionImage(actionElement));
+        });
+        user = currentUser();
       },
 
       update: function(event, action) {
