b0260861a32a5a870b1fbe6123af8990b4ef0176
Xiao Li <swing1979@gmail.com>
2012-05-02 09:35:07 -0700

    put plugin into an init function; removed macro plugin title for the behavior is confusing

--- src/js/wikimate_plugins.js
+++ src/js/wikimate_plugins.js
@@ -4,50 +4,52 @@
       return jQuery.nodeName(ele, tn);
     });
   };
-  var render_from_server_paragraph = {
-    // status: ['rendered', 'rendering', 'editing']
-    //   rendering => rendered
-    //   rendered => editing
-    //   editing => rendering
-    emit: function(div, item) {
-      if (item.text === "") {
-        div.data('status', 'rendered');
-        return div.empty();
-      } else {
-        div.data('status', 'rendering');
-        this.parents('.card_editor').card_editor('renderWiki', item.text, function(html) {
+  var render_from_server_paragraph = (function() {
+    return {
+      // status: ['rendered', 'rendering', 'editing']
+      //   rendering => rendered
+      //   rendered => editing
+      //   editing => rendering
+      emit: function(div, item) {
+        if (item.text === "") {
+          div.data('status', 'rendered');
+          return div.empty();
+        } else {
+          div.data('status', 'rendering');
+          this.parents('.card_editor').card_editor('renderWiki', item.text, function(html) {
+            if (div.data('status') == 'rendering') {
+              div.data('status', 'rendered');
+              div.find('img').remove();
+              div.append(html);
+            }
+          });
+          return div.html('<img src="/images/spinner.gif" title="loading..."/>');
+        }
+      },
+      bind: function(div, item) {
+        var $this = this;
+        div.bind('dblclick', function(e) {
+          if (clickableElement(e.target) || !$this.story_item('editable')) {
+            return;
+          }
           if (div.data('status') == 'rendering') {
-            div.data('status', 'rendered');
-            div.find('img').remove();
-            div.append(html);
+            alert("Please wait for server rendering " + item.text + "");
+          } else {
+            div.data('status', 'editing');
+            $this.story_item('edit');
           }
         });
-        return div.html('<img src="/images/spinner.gif" title="loading..."/>');
+      },
+      edit: function(item) {
+        return this.wikimate_text_editor('init');
       }
-    },
-    bind: function(div, item) {
-      var $this = this;
-      div.bind('dblclick', function(e) {
-        if (clickableElement(e.target) || !$this.story_item('editable')) {
-          return;
-        }
-        if (div.data('status') == 'rendering') {
-          alert("Please wait for server rendering " + item.text + "");
-        } else {
-          div.data('status', 'editing');
-          $this.story_item('edit');
-        }
-      });
-    },
-    edit: function(item) {
-      return this.wikimate_text_editor('init');
-    }
-  };
+    };
+  })();
 
   if (window.wikimate && window.wikimate.plugins) {
     $.extend(window.wikimate.plugins, {
       paragraph: render_from_server_paragraph,
-      macro: $.extend({title: 'Macro'}, render_from_server_paragraph),
+      macro: $.extend({}, render_from_server_paragraph),
       body_macro: $.extend({}, render_from_server_paragraph),
       html: $.extend({}, render_from_server_paragraph)
     });
